ddd69e22bcbb769c00c87c6fa3ace6040e2b555f
Gabriele Tozzi <gabriele@tozzi.eu>
2022-03-06 13:20:12 +0100

    Drafted unit tests and Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+# JABS linux based makefile
+
+tests:
+	python3 -m unittest tests
+	python2 -m unittest tests
---
+++ tests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+""" @package docstring
+JABS - Just Another Backup Script
+
+This file contains unit tests useful for developing purposes
+
+@author Gabriele Tozzi <gabriele@tozzi.eu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+class TestRun(unittest.TestCase):
+
+	def test_import(self):
+		import jabs
+
+
+if __name__ == '__main__':
+	unittest.main()
